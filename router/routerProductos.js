2fbe44bd53fa621acc50068ff26aa0c3956913f2
renedonaire <49369835+renedonaire@users.noreply.github.com>
2021-10-23 19:19:12 -0300

    Cleanup

--- router/routerProductos.js
+++ router/routerProductos.js
@@ -6,24 +6,23 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// const arrayProductos = JSON.parse('../data/productos.txt')
 const arrayProductos = [
   {
     "title": "La Vuelta al Mundo en 80 días",
     "price": 15900,
-    "thumbnail": "https://images.cdn3.buscalibre.com/fit-in/360x360/cc/ff/ccff1c289cdb7b75f0f6b15c09be499d.jpg",
+    "thumbnail": "https://images/vuelta.jpg",
     "id": 1
   },
   {
     "title": "Primera Persona Del Singular",
     "price": 19900,
-    "thumbnail": "https://www.antartica.cl/media/catalog/product/9/7/9789569961212_1.png?quality=80&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&format=jpeg",
+    "thumbnail": "https://images/primera.jpeg",
     "id": 2
   },
   {
     "title": "Ajuste De Cuentas",
     "price": 15000,
-    "thumbnail": "https://www.antartica.cl/media/catalog/product/9/7/9789569646867_1.png?quality=80&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&format=jpeg",
+    "thumbnail": "https://images/ajuste.jpeg",
     "id": 3
   }
 ]
@@ -34,8 +33,8 @@ routerProductos.get('/', (req, res) => {
 
 
 routerProductos.get('/:id', (req, res) => {
-  const { ident } = req.params
-  const result = arrayProductos[parseInt(ident) - 1]
+  const { id } = req.params
+  const result = arrayProductos[parseInt(id) - 1]
   result ?
     res.json({ result })
     :
