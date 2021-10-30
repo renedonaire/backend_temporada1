a2742766966eec699246d2efdec310cffa6201d6
renedonaire <49369835+renedonaire@users.noreply.github.com>
2021-10-30 14:19:53 -0300

    Funcional

--- router/routerProductos.js
+++ router/routerProductos.js
@@ -10,19 +10,19 @@ const arrayProductos = [
   {
     "title": "La Vuelta al Mundo en 80 días",
     "price": 15900,
-    "thumbnail": "https://images/vuelta.jpg",
+    "thumbnail": "https://www.antartica.cl/media/catalog/product/9/7/9788417127916_1.png?quality=80&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&format=jpeg",
     "id": 1
   },
   {
     "title": "Primera Persona Del Singular",
     "price": 19900,
-    "thumbnail": "https://images/primera.jpeg",
+    "thumbnail": "https://www.antartica.cl/media/catalog/product/9/7/9789569961212_1.png?quality=80&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&format=jpeg",
     "id": 2
   },
   {
     "title": "Ajuste De Cuentas",
     "price": 15000,
-    "thumbnail": "https://images/ajuste.jpeg",
+    "thumbnail": "https://www.antartica.cl/media/catalog/product/9/7/9789569646867_1.png?quality=80&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&format=jpeg",
     "id": 3
   }
 ]
